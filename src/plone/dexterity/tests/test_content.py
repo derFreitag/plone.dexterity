6f4f25ef974159ad67137b7eacfc0b0824742ea6
Asko Soukka <asko.soukka@iki.fi>
2017-01-29 21:16:04 +0200

    Make getattr optional

--- src/plone/dexterity/tests/test_content.py
+++ src/plone/dexterity/tests/test_content.py
@@ -15,6 +15,7 @@ from plone.dexterity.interfaces import IDexterityContainer
 from plone.dexterity.interfaces import IDexterityContent
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.dexterity.schema import SCHEMA_CACHE
+from plone.dexterity.utils import initialize_missing_attributes
 from plone.folder.default import DefaultOrdering
 from Products.CMFCore.interfaces import ISiteRoot
 from Products.CMFCore.interfaces import ITypesTool
@@ -269,7 +270,8 @@ class TestContent(MockTestCase):
         self.assertTrue(IMarker2.providedBy(item))
 
         # Subtypes provide field defaults.
-        self.assertEqual("baz", getattr(item, "baz", None))
+        self.assertNotEqual("baz", getattr(item, "baz", None))
+        # XXX: item cannot adapt to IBehavior2 so it shouldn't provide its fields?
 
         # We also need to ensure that the _v_ attribute doesn't hide any
         # interface set directly on the instance with alsoProvides() or
@@ -407,6 +409,7 @@ class TestContent(MockTestCase):
 
         SCHEMA_CACHE.invalidate("testtype")
 
+        initialize_missing_attributes(content)  # WIP
         self.assertEqual("foo_default", content.foo)
         self.assertEqual(None, content.bar)
         self.assertEqual("id", content.id)
@@ -429,6 +432,7 @@ class TestContent(MockTestCase):
 
         SCHEMA_CACHE.invalidate("testtype")
 
+        initialize_missing_attributes(content)  # WIP
         self.assertEqual("foo_default", content.foo)
         self.assertEqual(None, content.bar)
         self.assertEqual("id", content.id)
@@ -458,6 +462,7 @@ class TestContent(MockTestCase):
 
         SCHEMA_CACHE.invalidate("testtype")
 
+        initialize_missing_attributes(content)  # WIP
         self.assertEqual("id_testtype", content.foo)
         self.assertEqual(None, content.bar)
         self.assertEqual("id", content.id)
@@ -484,6 +489,7 @@ class TestContent(MockTestCase):
         SCHEMA_CACHE.invalidate("testtype")
 
         # Schema field masks contained item
+        initialize_missing_attributes(content)  # WIP
         self.assertEqual("foo_default", content.foo)
 
         # But we can still obtain an item
@@ -898,6 +904,9 @@ class TestContent(MockTestCase):
 
         # Ensure that the field of foo is not the same field, also attached to
         # bar.
+        initialize_missing_attributes(foo)  # WIP
+        initialize_missing_attributes(bar)  # WIP
+        initialize_missing_attributes(baz)  # WIP
         self.assertTrue(foo.listfield is not bar.listfield)
         self.assertTrue(foo.listfield is not baz.listfield)
         # And just to reinforce why this is awful, we'll ensure that updating
